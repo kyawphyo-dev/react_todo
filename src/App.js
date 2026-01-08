21c027f988094e9ff742ba3efc9ef85be5d2d40d
Kyaw Phyo Win <kyawphyowin242965@gmail.com>
2026-01-08 12:08:38 +0700

    add_todo_feature

--- src/App.js
+++ src/App.js
@@ -9,16 +9,32 @@ import { useEffect, useState } from "react";
 
 function App() {
   let [todos, setTodos] = useState([]);
+
+  //fetch todos from the server
   useEffect(() => {
     fetch("http://localhost:3001/todos")
       .then((res) => res.json())
       .then((todos) => setTodos(todos));
-  });
+  }, []);
+
+  //function to add a new todo
+  let addTodo = (todo) => {
+    //post the new todo to the server
+    fetch("http://localhost:3001/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+    });
+    //update the state
+    setTodos((prevTodos) => [...prevTodos, todo]);
+  };
   return (
     <div className="todo-app-container">
       <div className="todo-app">
         <h2>Todo App</h2>
-        <TodoForm />
+        <TodoForm addTodo={addTodo} />
 
         <TodoList todos={todos} />
 
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function TodoForm() {
+export default function TodoForm({ addTodo }) {
+  let [title, setTitle] = useState("");
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    //post the new todo to the server
+    if (!title.trim()) return;
+
+    const todo = {
+      id: Date.now(),
+      title: title.trim(),
+      completed: false,
+    };
+    addTodo(todo);
+    //clear the input field
+    setTitle("");
+  };
   return (
     <div>
-      <form action="#">
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           className="todo-input"
           placeholder="What do you need to do?"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
         />
       </form>
     </div>
